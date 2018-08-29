daef35c016465d6269f505f66be25fadb2dd68b5
Sonal Singh <sonal.singh.19993@gmail.com>
2018-08-29 14:07:51 +0000

    Added Prebuilt GCam Signed off By:- Sonal Singh <sonal.singh.19993@gmail.com>

--- device.mk
+++ device.mk
@@ -126,7 +126,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8937 \
-    Snap
+    libmm-qcamera \
+    mm-qcamera-app \
+    MGC
+
 
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
