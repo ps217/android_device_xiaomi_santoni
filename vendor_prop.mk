ed4d084bfd7520a1c200bea8996dc55662872c0e
Ronalds Ustinovs <normandija1945@gmail.com>
2018-12-22 16:37:57 +0000

    santoni: Enable DTS_EAGLE audio HAL features

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -40,6 +40,9 @@ vendor.voice.path.for.pcm.voip=true \
 vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
 vendor.voice.voip.conc.disabled=true
+vendor.audio.use.dts_eagle=true
+use.dts_eagle=true
+hpx_send_params=1
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
